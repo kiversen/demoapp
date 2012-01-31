24f7e5a0f3b24ec9eff187c481701fcb37e0b9fc
Kent Iversen <kent.iversen@webstep.no>
2012-01-31 16:38:56 +0100

    Lagt til validering og forbedret gui opplevelsen

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+	has_many :comments
+	
+		validates :title, :body, presence: true
+		validates :title, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Demoapp::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,6 +54,7 @@ Demoapp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
